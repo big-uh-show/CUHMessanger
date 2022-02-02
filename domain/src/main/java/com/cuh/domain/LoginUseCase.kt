14a8663b3557b4fb0db660f3781f58120cb8d1e3
choi_unhak <76737193+big-uh-show@users.noreply.github.com>
2022-02-02 17:03:48 +0900

    [22.02.02] Add Room

--- domain/src/main/java/com/cuh/domain/LoginUseCase.kt
+++ domain/src/main/java/com/cuh/domain/LoginUseCase.kt
@@ -1,9 +1,5 @@
 package com.cuh.domain
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-
 class LoginUseCase (private val userDataRepository: UserDataRepository){
 
     suspend fun invoke(id : String, pw: String):UserData {
