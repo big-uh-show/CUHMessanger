bc0fac7331e16c3862f72a6c0663654c177a70c8
choi_unhak <76737193+big-uh-show@users.noreply.github.com>
2022-02-09 22:53:04 +0900

    [22.02.09]

--- domain/src/main/java/com/cuh/domain/LoginUseCase.kt
+++ domain/src/main/java/com/cuh/domain/LoginUseCase.kt
@@ -4,5 +4,6 @@ class LoginUseCase (private val userDataRepository: UserDataRepository){
 
     suspend fun invoke(id : String, pw: String):UserData {
         return userDataRepository.login(id, pw)
+        // 1 day 1 commit에 매몰되지 않기
     }
 }
